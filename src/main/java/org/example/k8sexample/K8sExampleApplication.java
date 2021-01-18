93666599d49062c32f946cd1785a378fc70882f3
Ruslan Mustaev <mustaev.ruslan@hotmail.com>
2021-01-18 12:37:28 +0300

    Path fix

--- src/main/java/org/example/k8sexample/K8sExampleApplication.java
+++ src/main/java/org/example/k8sexample/K8sExampleApplication.java
@@ -17,7 +17,7 @@ public class K8sExampleApplication {
 
     @GetMapping("/")
     public String hello() throws IOException {
-        Path path = Paths.get("data", "fileTest.txt");
+        Path path = Paths.get("/data", "fileTest.txt");
         String text = Files.readAllLines(path).get(0);
         return "Hello from k8s-example service on " + InetAddress.getLocalHost() +
                 "<br>Storage content: " + text;
